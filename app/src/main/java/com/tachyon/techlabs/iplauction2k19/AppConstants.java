eb10180d688a072a72f1e6ba9874896ceddd4a69
Tachyon Tech Labs <tachyontechlabs@gmail.com>
2018-10-06 11:27:51 +0530

    Tachyon commit

--- app/src/main/java/com/tachyon/techlabs/iplauction2k19/AppConstants.java
+++ app/src/main/java/com/tachyon/techlabs/iplauction2k19/AppConstants.java
@@ -1,7 +1,5 @@
 package com.tachyon.techlabs.iplauction2k19;
 
 public class AppConstants {
-
-    public static final String PREFS = "techrace_prefs";
-    public static final String PREFS_UNLOCKED = "unlocked";
+    
 }
